6b07085e3f94a5453773b520e4f1bbf183085795
SantiagoAral <124814846+SantiagoAral@users.noreply.github.com>
2024-08-21 12:59:07 -0500

    Add files via upload

---
+++ src/main/java/edu/unisabana/dyas/patterns/Client.java
@@ -0,0 +1,14 @@
+package edu.unisabana.dyas.patterns;
+
+import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayer;
+import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayerAdapter;
+import edu.unisabana.dyas.patterns.utils.AudioPlayer;
+
+public class Client {
+    public static void main(String[] args) {
+        AudioPlayer player = new AdvancedAudioPlayerAdapter(new AdvancedAudioPlayer());
+        player.play("mp4", "video.mp4");
+        player.play("vlc", "video.vlc");
+        player.stop();
+    }
+}
---
+++ src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
@@ -0,0 +1,27 @@
+
+package edu.unisabana.dyas.patterns.utils;
+
+// Adaptador que implementa la interfaz AudioPlayer y utiliza AdvancedAudioPlayer
+public class AdvancedAudioPlayerAdapter implements AudioPlayer {
+    private AdvancedAudioPlayer advancedAudioPlayer;
+
+    public AdvancedAudioPlayerAdapter(AdvancedAudioPlayer advancedAudioPlayer) {
+        this.advancedAudioPlayer = advancedAudioPlayer;
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp4")) {
+            advancedAudioPlayer.playMp4(fileName);
+        } else if (audioType.equalsIgnoreCase("vlc")) {
+            advancedAudioPlayer.playVlc(fileName);
+        } else {
+            System.out.println("Formato no soportado: " + audioType);
+        }
+    }
+
+    @Override
+    public void stop() {
+        advancedAudioPlayer.stop();
+    }
+}
